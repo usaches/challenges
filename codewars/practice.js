eeb841dd7a8d9055abf48408c101435925fae089
usaches <usaches@gmail.com>
2018-12-04 19:53:04 -0500

    jj

--- codewars/practice.js
+++ codewars/practice.js
@@ -78,3 +78,20 @@ function minMax(arr){
     var str = arr2;
     return [parseFloat(Math.min(...str)), parseFloat(Math.max(...str))]; // fix me!
 }
+
+class SequenceSum {
+    static showSequence(count) {
+        if (count === 0) return '0=0'
+        if (count < 0) return `${count}<0`
+
+        const numberRange = Array.from({ length: count + 1 }, (_, index) => index)
+
+        const summatoryString = numberRange.join('+')
+        const total = numberRange.reduce((total, value) => total + value, 0)
+
+        return `${summatoryString} = ${total}`
+    }
+}
+
+// Function Export
+module.exports = SequenceSum;
