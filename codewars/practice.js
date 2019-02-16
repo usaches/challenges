5041bd2040ddd9172d695f3d2ad79d5533e95517
usaches <usaches@gmail.com>
2019-02-15 19:42:41 -0500

    jaden

--- codewars/practice.js
+++ codewars/practice.js
@@ -147,3 +147,9 @@ class SmallestIntegerFinder {
         return small;
     }
 }
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(function (item) {
+        return item.replace(item.charAt(0), item.charAt(0).toUpperCase());
+    }).join(' ');
+};
