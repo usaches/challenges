0ee5fd11f3a5fdcb025fa65bcd19f3baf7acae2c
usaches <usaches@gmail.com>
2019-02-21 17:26:46 -0500

    hh

---
+++ codewars/reactpractice.js
@@ -0,0 +1,12 @@
+// Objective: Fill in the boilerplate React code required to render an
+// unordered list (<ul>) to the page. The list should contain 3 list items
+// (<li>) with anything in them you want.
+
+// HINTS:
+// import the libraries you need first
+// use one of the libraries to render some JSX to the page
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<div><ul><li>1</li><li>1</li><li>1</li></ul></div>, document.getElementById("root"))
