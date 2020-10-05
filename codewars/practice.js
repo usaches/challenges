012df77ed975c3daf0002f5ae3363373a8d23a49
usaches <38760409+usaches@users.noreply.github.com>
2020-10-05 15:28:03 -0400

    Update practice.js

--- codewars/practice.js
+++ codewars/practice.js
@@ -243,3 +243,23 @@ var numArray = [140, 1400, 14000, 90];
 
 numArray.sort(sortNumber);
 numArray.join(",");
+
+
+function lcm_two_numbers(x, y) {
+   if ((typeof x !== 'number') || (typeof y !== 'number')) 
+    return false;
+  return (!x || !y) ? 0 : Math.abs((x * y) / gcd_two_numbers(x, y));
+}
+
+function gcd_two_numbers(x, y) {
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}
+console.log(lcm_two_numbers(3,15));
+console.log(lcm_two_numbers(10,15));
